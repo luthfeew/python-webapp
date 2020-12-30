12fb61d76de3ec7b9e53758744fcaa705039f350
luthfeew <luthfeew@gmail.com>
2020-12-30 14:45:20 +0700

    app work

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
+import sqlite3
+
 application = Flask(__name__)
+
 
 @application.route('/')
 def main():
-    return render_template('index.html')
+    con = sqlite3.connect('database.db')
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("SELECT * FROM berita LIMIT 3")
+    rows = cur.fetchall()
+    return render_template('index.html', rows=rows)
+
 
 if __name__ == '__main__':
     application.run(debug=True)
